ba9195581016e9080be050dcdaee7abe1db574d3
Drew <drew@drew.attlocal.net>
2021-03-05 17:39:12 -0500

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,102 @@
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs')
+
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+const app = express()
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup hendlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Drew Michael'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Drew Michael'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        helpMessage: 'Enter a location for weather',
+        name: 'Drew Michael'
+    })
+})
+
+app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an address'
+        })
+    }
+
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({ error })
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
+
+            res.send({
+                location: location,
+                forecast: forecastData,
+                address: req.query.address
+            })
+        })
+    })
+})
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+
+    console.log(req.query.search)
+    res.send({
+        products: []
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404 Page Not Found',
+        errorMessage: 'Help article was not found',
+        name: 'Drew Michael'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404 Page Not Found',
+        errorMessage: 'Page Not Found',
+        name: 'Drew Michael'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000')
+})
