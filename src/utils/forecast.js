25256ef9dd608a0917ed7d9cf810dfaa1a1121d6
Drew <drew@drew.attlocal.net>
2021-03-05 19:14:47 -0500

    added visibility to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -4,12 +4,13 @@ const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=f6757984088026b357e486756b3419a3&query=' + latitude + ',' + longitude + '&units=f'
 
     request({ url, json: true }, (error, { body }) => {
+        console.log(body)
         if (error) {
             callback('Unable to connect to weather service!', undefined)
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out`)
+            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out. Current visibility is around ${body.current.visibility}%.`)
         }
     })
 
